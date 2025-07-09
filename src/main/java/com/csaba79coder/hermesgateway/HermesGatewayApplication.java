58983a0b2af2322349d7414c46436321c2b041aa
Csaba Vadász <csabavadasz79@gmail.com>
2025-07-09 13:06:03 +0200

    Initial commit

---
+++ src/main/java/com/csaba79coder/hermesgateway/HermesGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.hermesgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HermesGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HermesGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/csaba79coder/hermesgateway/HermesGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csaba79coder.hermesgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HermesGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
